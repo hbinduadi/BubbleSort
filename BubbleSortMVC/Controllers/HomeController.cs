2b9c338b4592bca0ee80419f28786c1d73df198b
hbinduadi <42518596+hbinduadi@users.noreply.github.com>
2018-10-25 22:13:26 -0400

    Add files via upload

---
+++ BubbleSortMVC/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BubbleSortMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            int[] BubSort = new int[10];
+            Random rnd = new Random();
+            for(int counter=0; counter <10; counter++)
+            {
+                BubSort[counter] = rnd.Next(1, 100);
+            }
+            bool swapped;
+            do
+            {
+                swapped = false;
+                for (int count = 0; count < BubSort.Length - 1; count++)
+                {
+                    if (BubSort[count] > BubSort[count + 1])
+                    {
+
+                        int temp = BubSort[count + 1];
+                        BubSort[count + 1] = BubSort[count];
+                        BubSort[count] = temp;
+                        swapped = true;
+                    }
+                }
+
+            } while (swapped == true);
+            ViewBag.bubsort = BubSort;
+            return View();
+        }
+    }
+}
